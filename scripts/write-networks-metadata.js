6471217370b036400fa827f5ee2bf446d2c5afcb
Luca Milanese <luca.milanese90@gmail.com>
2023-01-12 11:00:57 +0100

    refactor: extract networks metadata config

--- scripts/write-networks-metadata.js
+++ scripts/write-networks-metadata.js
@@ -2,42 +2,20 @@
 'use strict'
 
 const axios = require('axios')
+const { networksMetadata } = require('../networksMetadata.config')
+const { GEN_X_NETWORK_ID } = require('../chains.config')
 
 // https://github.com/ethereum-lists/chains
 const chainDataUrl = 'https://chainid.network/chains.json'
 
 axios(chainDataUrl).then((response) => {
-  response.data.push({
-    name: 'Gaia-X Testnet',
-    chain: 'GX',
-    network: 'testnet',
-    rpc: ['https://rpc.gaiaxtestnet.oceanprotocol.com'],
-    faucets: [],
-    nativeCurrency: {
-      name: 'Gaia-X',
-      symbol: 'GX',
-      decimals: 18
-    },
-    infoURL: 'https://gaia-x.eu',
-    shortName: 'GX',
-    chainId: 2021000,
-    networkId: 2021000
-  })
-  response.data.push({
-    name: 'Catena-X Testnet',
-    chain: 'CX',
-    network: 'testnet',
-    rpc: ['https://rpc.catenaxtestnet.oceanprotocol.com'],
-    faucets: [],
-    nativeCurrency: {
-      name: 'Catena-X',
-      symbol: 'CX',
-      decimals: 18
-    },
-    infoURL: 'https://catena-x.net',
-    shortName: 'CX',
-    chainId: 2021001,
-    networkId: 2021001
-  })
-  process.stdout.write(JSON.stringify(response.data, null, '  '))
+  // avoid having 2 nodes with the same chainId
+  const filteredData = response.data.filter(
+    (node) => node.chainId !== GEN_X_NETWORK_ID
+  )
+
+  // add custom networks metadata to the list
+  const fullNetworksMetadata = filteredData.concat(networksMetadata)
+
+  process.stdout.write(JSON.stringify(fullNetworksMetadata, null, '  '))
 })
